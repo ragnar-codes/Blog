b8a7c9d064f113637529fcf3e1036a05415e924f
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 19:11:29 +0200

    show articles

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require('./models/article.js')
 const articleRouter = require('./routes/articles.js')
 const app = express();
 // connect the database
@@ -10,25 +11,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Articles1',
-        createdAt: new Date(),
-        description: 'test description1'
-
-    },
-    {
-        title: 'Test Articles2',
-        createdAt: new Date(),
-        description: 'test description2'
-
-    },
-    {
-        title: 'Test Articles3',
-        createdAt: new Date(),
-        description: 'test description3'
-
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find()
     res.render('articles/index', {articles: articles});
 })
 
