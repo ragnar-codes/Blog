ca338800124b998b12f43507d20f1e783cb269b6
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 22:08:22 +0200

    add delte button with method-override

--- routes/articles.js
+++ routes/articles.js
@@ -25,6 +25,9 @@ router.post('/', async (req, res) => {
     
 })
 
-
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+})
 module.exports = router
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article.js')
 const articleRouter = require('./routes/articles.js')
+const methodOverride = require('method-override')
 const app = express();
 // connect the database
 mongoose.connect('mongodb://localhost/blog', {
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/blog', {
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 
 app.get('/', async (req, res) => {
