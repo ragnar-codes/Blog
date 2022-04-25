1cd4e7b9eee1770d9b30c1c312a18cdfed1769bf
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 17:11:37 +0200

    demo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+
+
+app.listen(5000, () => console.log('Server running on port 5000'));
