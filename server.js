d756c1f39db324bf2d1a186c4e2e0a88c46a21c6
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-25 17:26:50 +0200

    set up router

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const articleRouter = require('./routes/articles.js')
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
     res.render('index');
