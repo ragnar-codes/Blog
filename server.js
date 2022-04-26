e3598e73ef53e5d684b4a1f088d1f24a4bc6453c
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-04-26 22:33:38 +0200

    final

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Article = require('./models/article.js')
-const articleRouter = require('./routes/articles.js')
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
-const app = express();
-// connect the database
-mongoose.connect('mongodb://localhost/blog', {
-    useNewUrlParser: true, useUnifiedTopology: true});
+const app = express()
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
-
-
-app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({createdAt: 'desc'})
-    res.render('articles/index', {articles: articles});
+mongoose.connect('mongodb://localhost:27017/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
-app.use('/articles', articleRouter);
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
